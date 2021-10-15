7adc60a4de6ae564860633027a9880a7acd2f6ae
theo-quezelperron <91187677+theo-quezelperron@users.noreply.github.com>
2021-10-15 09:51:14 +0200

    Initial commit
    Ajout des fichiers de bases + réponses aux questions 1 et 2 de l'étape 1.

---
+++ monApplication/controller/mainController.php
@@ -0,0 +1,26 @@
+<?php
+
+class mainController
+{
+
+	public static function helloWorld($request,$context)
+	{
+		$context->mavariable="hello world";
+		return context::SUCCESS;
+	}
+
+
+
+	public static function index($request,$context){
+		
+		return context::SUCCESS;
+	}
+
+	public static function superTest($request, $context){
+		$context->param1 = $_GET['param1'];
+		$context->param2 = $_GET['param2'];
+		return context::SUCCESS;
+	}
+
+
+}
---
+++ monApplication/view/superTestSuccess.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+        <p>J'ai compris: <?php echo $context->param1;?><br>
+        Super: <?php echo $context->param2;?><br>
+        </p>
+    </body>
+</html>
