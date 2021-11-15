54c28bf5c2e25a393f35d42d60041db701712609
theo-quezelperron <91187677+theo-quezelperron@users.noreply.github.com>
2021-11-15 16:47:10 +0100

    Correction bug du a mauvaise manipulation

--- monApplication/view/recherche2Success.php
+++ monApplication/view/recherche2Success.php
@@ -1,6 +1,23 @@
 
 <ul> 
   <?php foreach( $context->trajet as $data ): ?>
-  <li><?php var_dump($data) ?></li> 
+  <li><?php echo $data["id"] ?></li> 
   <?php endforeach; ?>
 </ul>
+
+
+<table>
+    <thead>
+        <tr>
+            <th>Départ</th>
+            <th>Départ</th>
+            <th>Départ</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>The table body</td>
+            <td>with two columns</td>
+        </tr>
+    </tbody>
+</table>
