fcd76b76cb289e1d4d761fe84045b3a76fd290d6
theo-quezelperron <91187677+theo-quezelperron@users.noreply.github.com>
2021-11-15 16:20:31 +0100

    Correction structure s3

--- monApplication.php
+++ monApplication.php
@@ -11,6 +11,7 @@ $action =  $_REQUEST['action'];
 require_once 'lib/core.php';
 require_once $nameApp.'/controller/mainController.php';
 
+
 foreach(glob($nameApp.'/model/*.class.php') as $model)
 	include_once $model ;   
 
--- monApplication/controller/mainController.php
+++ monApplication/controller/mainController.php
@@ -31,4 +31,14 @@ class mainController
       return context::SUCCESS;
 	}
 
+	public static function recherche($request, $context){
+        
+        return context::SUCCESS;
+    }
+
+    public static function recherche2($request, $context){
+        $context->trajet = trajetTable::getTrajet( $_GET['depart'],$_GET['arrivee']);
+        return context::SUCCESS;
+    }
+
 }
