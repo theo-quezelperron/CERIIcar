855857d94594723ac12eeae80be8a7a4e9babdfd
theo <theo.quezel-perron@alumni.univ-avignon.fr>
2021-12-12 16:23:22 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,23 +12,19 @@ $("#btn_1").on("click", function(){
         switch (parsed_content.bandeau.class) {
           case "Alerte":
             $("#bandeau").removeClass();
-            $("#bandeau").addClass("alert");
-            $("#bandeau").addClass("alert-danger");
+            $("#bandeau").addClass("alert alert-danger");
             break;
           case "Warning":
             $("#bandeau").removeClass();
-            $("#bandeau").addClass("alert");
-            $("#bandeau").addClass("alert-warning");
+            $("#bandeau").addClass("alert alert-warning");
             break;
           case "Réussite":
             $("#bandeau").removeClass();
-            $("#bandeau").addClass("alert");
-            $("#bandeau").addClass("alert-success");
+            $("#bandeau").addClass("alert alert-success");
             break;
           default :
             $("#bandeau").removeClass();
-            $("#bandeau").addClass("alert");
-            $("#bandeau").addClass("alert-primary");
+            $("#bandeau").addClass("alert alert-primary");
             break;
         }
         $("#bandeau").html(parsed_content.bandeau.value);
