9e72e6492590237d9134299f1cc2b20d50d4ee44
theo <theo.quezel-perron@alumni.univ-avignon.fr>
2021-12-12 15:43:04 +0100

    Update bandeau.php

--- monApplication/view/bandeau.php
+++ monApplication/view/bandeau.php
@@ -1,28 +1,29 @@
 <?php 
-if ($context->alerts != null){
-    foreach ($context->alerts as $key => $value){
-        switch ($key) {
-            case "Warning":
-                echo '<div id="bandeau" class="alert alert-warning" role="alert" style="visibility: hidden">'. $value .'</div>';
-                break;
-            case "Alerte":
-                echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">'. $value .'</div>';
-                break;
-            case "Réussite":
-                echo '<div id="bandeau" class="alert alert-success" role="alert" style="visibility: hidden">'. $value .'</div>';
-                break;
-            case null:
-                echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">'. $value .'</div>';
-                break;
-            default:
-                echo '<div id="bandeau" class="alert alert-primary" role="alert" style="visibility: hidden">'. $value .'</div>';
-                break;
-        }
-    }
-}
-else{
-    echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">ERREUR!</div>';
-}
+// if ($context->alerts != null){
+//     foreach ($context->alerts as $key => $value){
+//         switch ($key) {
+//             case "Warning":
+//                 echo '<div id="bandeau" class="alert alert-warning" role="alert" style="visibility: hidden">'. $value .'</div>';
+//                 break;
+//             case "Alerte":
+//                 echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">'. $value .'</div>';
+//                 break;
+//             case "Réussite":
+//                 echo '<div id="bandeau" class="alert alert-success" role="alert" style="visibility: hidden">'. $value .'</div>';
+//                 break;
+//             case null:
+//                 echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">'. $value .'</div>';
+//                 break;
+//             default:
+//                 echo '<div id="bandeau" class="alert alert-primary" role="alert" style="visibility: hidden">'. $value .'</div>';
+//                 break;
+//         }
+//     }
+// }
+// else{
+//     echo '<div id="bandeau" class="alert alert-danger" role="alert" style="visibility: hidden">ERREUR!</div>';
+// }
 
+echo '<div id="bandeau" class="alert " role="alert" style="visibility: hidden"></div>';
 
 ?>
