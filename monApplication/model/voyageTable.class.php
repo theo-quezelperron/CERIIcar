fb2a86cca79da26dae5dd65d27e4fe7062fa051b
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-08 23:53:06 +0100

    Update voyageTable.class.php

--- monApplication/model/voyageTable.class.php
+++ monApplication/model/voyageTable.class.php
@@ -34,7 +34,7 @@ class voyageTable {
         }
         if (empty($query))
         {
-            return null;
+            return 0;
         }
         return $query->fetchAll();
     }
@@ -55,7 +55,7 @@ class voyageTable {
         }
         if (empty($query))
         {
-            return null;
+            return 0;
         }
         return $query->fetchAll(); // retourne un tableau d'enregistrements (tableau de tableaux de valeurs)
 
