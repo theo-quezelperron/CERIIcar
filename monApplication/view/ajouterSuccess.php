4eb38965f64d80963e0869fcaf491cd42b4560b5
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-10 00:11:40 +0100

    Update ajouterSuccess.php

--- monApplication/view/ajouterSuccess.php
+++ monApplication/view/ajouterSuccess.php
@@ -17,22 +17,15 @@
         </select>
     </div>
     <div class="mb-3">
-        <label for="nbplace">Nombre de places: </label>
-        <select class="form-select" id="nbplace" name="nbplace" required>
-        <option value="1" selected>1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6">6</option>
-        </select>
+        <label for="exampleInputUser1" class="form-label">Nombre de places</label>
+        <input class="reserverPlace" type="number" name="nbplce" value="1" min="1" max="100">
     </div>
     <div class="mb-3">
         <label for="exampleInputUser1" class="form-label">Heure de départ</label>
-        <input class="reserverPlace" type="number" name="hdepart" value="1" min="1" max="100">
+        <input class="reserverPlace" type="number" name="hdepart" value="1" min="1" max="24">
     </div>
     <div class="mb-3">
-        <label for="exampleInputUser1" class="form-label">Contraintes/label>
+        <label for="exampleInputUser1" class="form-label">Contraintes</label>
         <input type="text" class="form-control" id="contrainte" name="contrainte" aria-describedby="userHelp">
     </div>
 </form>
