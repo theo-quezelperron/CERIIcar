396bf3b56f373e8d7023cce28338ce2df65547a3
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-09 19:13:52 +0100

    Update signinError.php

--- monApplication/view/signinError.php
+++ monApplication/view/signinError.php
@@ -1,1 +1,23 @@
-<p>Erreur veuillez réssayer</p>
+
+<div id="htmlResult">
+<h3>Erreur veuillez réssayer</h3>
+<form id="enregistrement">
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Nom d'utilisateur</label>
+        <input type="text" class="form-control" id="pseudo" name="pseudo" aria-describedby="userHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Nom</label>
+        <input type="text" class="form-control" id="nom" name="nom" aria-describedby="userHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Prénom</label>
+        <input type="text" class="form-control" id="prenom" name="prenom" aria-describedby="userHelp">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" id="pass" name="pass">
+    </div>
+</form>
+<button class="btn btn-primary enregistrement">S'enregistrer</button>
+</div>
