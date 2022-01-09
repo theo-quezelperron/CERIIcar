eaf7c15986cc015a752aea486db65a951ab85954
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-09 19:40:09 +0100

    ob fix

--- monApplication/view/detailCorresSuccess.php
+++ monApplication/view/detailCorresSuccess.php
@@ -31,9 +31,8 @@ if (!is_null($context->voyages)){
       <td><?php echo $data["prenom"]; ?></td>
       <td><?php echo $data["contraintes"]; ?></td>
       <?php 
-      var_dump($context->isLogged);
       var_dump($context->session);
-      if($context->getSessionAttribute('id')){
+      if(!empty($context->session)){
           echo '<td><input class="reserverPlace" type="number" name="nbplace" value="1" min="1" max="100"></td>';
           echo '<td><button class="btn btn-primary reserver" id=' . $data["id"] . '">Réserver</button></td>';
       }?>    
