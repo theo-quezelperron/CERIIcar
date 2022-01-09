c12667733c74d288712cb59b001f2380ede1044a
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-09 21:34:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 $("#btn_1").on("click", function(){
     console.log("clique");
-    var x = document.getElementById('isLogged');
-    console.log(x);
-    let urlString = "ajaxDispatcher.php?action=tableau&depart=" + $("#depart").val() + "&arrivee=" + $("#arrivee").val() + "&nbplace=" + $("#nbplace").val() + "&correspondance=" + ($("#correspondance").is(":checked") ? $("#correspondance").val() : 'false') + "&isLogged=" + x.dataset.value;
+    var y = document.getElementById('isLogged');
+    console.log(y);
+    let urlString = "ajaxDispatcher.php?action=tableau&depart=" + $("#depart").val() + "&arrivee=" + $("#arrivee").val() + "&nbplace=" + $("#nbplace").val() + "&correspondance=" + ($("#correspondance").is(":checked") ? $("#correspondance").val() : 'false') + "&isLogged=" + y.dataset.value;
     let test = $.ajax({
       url: urlString,
       method: "GET",
