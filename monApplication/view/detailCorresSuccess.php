f2e9c02f713721455368aa1c8f42b094c0e36674
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-09 18:48:03 +0100

    Update detailCorresSuccess.php

--- monApplication/view/detailCorresSuccess.php
+++ monApplication/view/detailCorresSuccess.php
@@ -32,7 +32,7 @@ if (!is_null($context->voyages)){
       <td><?php echo $data["contraintes"]; ?></td>
       <?php 
       //var_dump($_SESSION);
-      if(!empty($_SESSION["id"])){
+      if($context->isLogged){
           echo '<td><input class="reserverPlace" type="number" name="nbplace" value="1" min="1" max="100"></td>';
           echo '<td><button class="btn btn-primary reserver" id=' . $data["id"] . '">Réserver</button></td>';
       }?>    
