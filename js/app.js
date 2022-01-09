dd5e9271751addda60be27196f63c6094330e6e5
theo <90631390+AzuraGit@users.noreply.github.com>
2022-01-09 01:52:30 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,10 +7,9 @@ $("#btn_1").on("click", function(){
       processData: false,
       contentType: false,
       success: function(result){
-        let parsed_content = JSON.parse(result);
         console.log(result);
-        $("#htmlResult").html(parsed_content.corps);
-        switch (parsed_content.bandeau.class) {
+        $("#htmlResult").html(result.corps);
+        switch (result.bandeau.class) {
           case "Alerte":
             $("#bandeau").removeClass();
             $("#bandeau").addClass("alert alert-danger fade show");
@@ -28,7 +27,7 @@ $("#btn_1").on("click", function(){
             $("#bandeau").addClass("alert alert-primary fade show");
             break;
         }
-        $("#bandeau").html(parsed_content.bandeau.value);
+        $("#bandeau").html(result.bandeau.value);
         $("#bandeau").append('<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>');
          
         document.getElementsByClassName("alert")[0].style.display = "block";
@@ -39,7 +38,7 @@ $("#btn_1").on("click", function(){
       },
       error: function(result){
         let parsed_content = JSON.parse(result);
-        $("#htmlResult").html(parsed_content.corps);
+        $("#htmlResult").html(result.corps);
         document.getElementsByClassName("alert")[0].style.display = "block";
         setTimeout(function(){
           document.getElementsByClassName("alert")[0].style.display = "none";// or fade, css display however you'd like.
