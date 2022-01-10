4b9944605e73d9bb50dfe36914d0f8ab28820984
theo-quezelperron <91187677+theo-quezelperron@users.noreply.github.com>
2022-01-10 16:27:34 +0100

    Ajax on ajouter voyage

--- monApplication/view/ajouterValideSuccess.php
+++ monApplication/view/ajouterValideSuccess.php
@@ -1,1 +1,37 @@
-<?php var_dump($context->op);
+<div id="htmlResult">
+<form action="monApplication.php?action=ajouterValide" method="post">
+<div class="mb-3">
+    <label for="depart">ville de départ: </label>
+        <select class="form-select" id="depart" name="depart" required>
+        <?php foreach( $context->villes["depart"] as $ville ): ?>
+        <option value="<?php echo $ville;?>"><?php echo $ville;?></option>
+        <?php endforeach ?>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label for="arrivee">ville d'arrivée: </label>
+        <select class="form-select" id="arrivee" name="arrivee" required>
+        <?php foreach( $context->villes["arrivee"] as $ville ): ?>
+        <option value="<?php echo $ville;?>"><?php echo $ville;?></option>
+        <?php endforeach ?>
+        </select>
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Nombre de places</label>
+        <input class="reserverPlace" type="number" name="nbplace" value="1" min="1" max="100">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Heure de départ</label>
+        <input class="reserverPlace" type="number" name="hdepart" value="1" min="1" max="24">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Tarif</label>
+        <input class="reserverPlace" type="number" name="tarif" value="10" min="1" max="500">
+    </div>
+    <div class="mb-3">
+        <label for="exampleInputUser1" class="form-label">Contraintes</label>
+        <input type="text" class="form-control" id="contrainte" name="contrainte" aria-describedby="userHelp">
+    </div>
+    <button class="btn btn-primary" type="submit">Ajouter</button>
+</form>
+</div>
